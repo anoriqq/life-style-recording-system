a53dd4d94daba7d53294c4cf88920e461292e30c
show0317sw <tunakan_8584@yahoo.co.jp>
2018-11-16 22:37:03 +0900

    ✨ GitHub認証機能を追加

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ passport.use(new GitHubStrategy({
 }));
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var logoutRouter = require('./routes/logout');
 
 var app = express();
 app.use(helmet());
@@ -47,12 +48,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({secret: 'cbc38509dd325300', resave: false, saveUninitialized: false}));
+app.use(session({secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false}));
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/logout', logoutRouter);
+
+app.get('/auth/github',
+  passport.authenticate('github', {scope: ['user:email']}),
+  function(req, res){
+    res.redirect('/');
+  }
+);
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', {failureRedirect:'/login'}),
+  function(req, res){
+    res.redirect('/');
+  }
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
