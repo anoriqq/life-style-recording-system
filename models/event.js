8b05b6deaa49324dbf1ab880e6b5a15a5c4beaef
show0317sw <tunakan_8584@yahoo.co.jp>
2018-11-17 01:11:27 +0900

    ✨ イベントのデータモデルを実装

---
+++ models/event.js
@@ -0,0 +1,30 @@
+'use strict';
+const loader = require('./sequelize-loader');
+const Sequelize = loader.Sequelize;
+
+const Event = loader.database.define('event', {
+  eventId: {
+    type: Sequelize.UUID,
+    primaryKey: true,
+    allowNull: false
+  },
+  status: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  startAt: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  endAt: {
+    type: Sequelize.DATE,
+    allowNull: true
+  },
+  duration: {
+    type: Sequelize.DATE,
+    allowNull: true
+  }
+}, {
+  freezeTableName: true,
+  timestamps: false
+});
