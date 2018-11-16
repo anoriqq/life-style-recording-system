741166b526ccfb85fdde45b627efbcf738e33b58
show0317sw <tunakan_8584@yahoo.co.jp>
2018-11-16 23:26:46 +0900

    ✨ passport-stubを導入し､テスト(ログイン時はユーザー名が表示される)を追加

--- test/test.js
+++ test/test.js
@@ -1,13 +1,30 @@
 'use strict';
 const request = require('supertest');
 const app = require('../app');
+const passportStub = require('passport-stub');
 
 describe('/login', ()=>{
+  before(()=>{
+    passportStub.install(app);
+    passportStub.login({username: 'testuser'});
+  });
+  after(()=>{
+    passportStub.logout();
+    passportStub.uninstall(app);
+  });
+
   it('ログインのためのリンクが含まれる', (done)=>{
     request(app)
       .get('/login')
       .expect('Content-Type', 'text/html; charset=utf-8')
       .expect(/<a href="\/auth\/github"/)
+      .expect(200, done);
+  });
+
+  it('ログイン時はユーザー名が表示される', (done)=>{
+    request(app)
+      .get('/login')
+      .expect(/testuser/)
       .expect(200, done);
   });
 });
